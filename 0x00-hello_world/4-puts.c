766b4a5a6afc4c81203dc11af9c871f090f499ae
Amuwa Joshua <amuwadunsin@gmail.com>
2022-10-13 13:06:12 -0700

    hw

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.hí>
+/**
+ * main - Entry poin
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
