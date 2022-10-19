5b2c512aefe00e141b50624710a400443e63424a
Amuwa Joshua <amuwadunsin@gmail.com>
2022-10-19 13:41:34 -0700

    loop

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,6 +8,7 @@
 int main(void)
 {
 	long int n1, n2, fn, afn;
+
 	n1 = 1;
 	n2 = 2;
 	fn = afn = 0;
@@ -17,7 +18,7 @@ int main(void)
 		n1 = n2;
 		n2 = fn;
 		if ((n1 % 2) == 0)
-		}
+		{
 			afn += n1;
 		}
 	}
